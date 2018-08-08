aaac3da052eede56f9b331a47c51a1150f49f26a
Diana Vulcu <vulcu.diana@gmail.com>
2018-08-08 23:48:28 +0300

    Added json loading functionality

---
+++ app.js
@@ -0,0 +1,8 @@
+function loadDoc() {
+    console.log("am intrat in loadDoc");
+    var userdata = JSON.parse(data);	
+    console.log(userdata[0].nume)
+    console.log(userdata[1].nume)	
+    console.log(userdata[2].nume)	    
+    console.log(userdata[3].nume)
+  }
