7bd176e99f754c8b3d17c00ceefcbbb6b7e309a5
Diana Vulcu <vulcu.diana@gmail.com>
2018-09-03 22:34:04 +0300

    am adaugat cele 4 tabele in html, sa mearga cu drag&drop'

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ var userdata = ""
 function loadDoc() {
     console.log("am intrat in loadDoc");
     userdata = JSON.parse(data);	
-    for (i=0; i<4;i++)
+    for (i=0; i<8;i++)
       {
+        console.log(userdata[i].nume);
         document.getElementById("testme"+i).innerHTML = userdata[i].nume;
       }
+
+  }
+  function modifyMe() {
+    console.log("am intrat in modifyMe");
+    userdata[0].nume = "gigi"
 
   }
