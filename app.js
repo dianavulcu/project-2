ed68d21b660b854c71d4007f9d90b8006e7ffdab
Diana Vulcu <vulcu.diana@gmail.com>
2018-09-03 23:21:26 +0300

    Adaugat functionalitati noi, reset si getIndex of sortable

--- app.js
+++ app.js
@@ -2,15 +2,23 @@ var userdata = ""
 function loadDoc() {
     console.log("am intrat in loadDoc");
     userdata = JSON.parse(data);	
-    for (i=0; i<8;i++)
-      {
-        console.log(userdata[i].nume);
-        document.getElementById("testme"+i).innerHTML = userdata[i].nume;
-      }
-
+    refresh()
   }
+  function refresh(){
+  for (i=0; i<8;i++)
+  {
+    console.log(userdata[i].nume);
+    document.getElementById("testme"+i).innerHTML = userdata[i].nume;
+  }
+}
   function modifyMe() {
     console.log("am intrat in modifyMe");
-    userdata[0].nume = "gigi"
-
+    userdata[0].nume = "Diana somnoroasa"
+    refresh()
+  }
+  
+  function resetMe() {
+    console.log("am intrat in resetMe");
+    $( "#sortable" ).sortable( "cancel" );
+    //$( "#sortable" ).sortable( "refresh" );
   }
