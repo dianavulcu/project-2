869c5d2e56b82253bf289d79486987763b133464
Diana Vulcu <vulcu.diana@gmail.com>
2018-09-09 15:27:47 +0300

    Am adaugat functii de permutare si swap - mai ramane o functie de terminat

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-var userdata = ""
+var userdata_model = ""
+var userdata_backend = ""
 function loadDoc() {
     console.log("am intrat in loadDoc");
-    userdata = JSON.parse(data);	
+    userdata_model = JSON.parse(data);	
+    userdata_backend = userdata_model
     refresh()
   }
   function refresh(){
   for (i=0; i<8;i++)
   {
-    console.log(userdata[i].nume);
-    document.getElementById("testme"+i).innerHTML = userdata[i].nume;
+    console.log(userdata_model[i].nume);
+    document.getElementById("testme"+i).innerHTML = userdata_model[i].nume;
   }
 }
   function modifyMe() {
     console.log("am intrat in modifyMe");
-    userdata[0].nume = "Diana somnoroasa"
+    userdata_model[0].nume = "Diana somnoroasa"
     refresh()
   }
   
@@ -21,4 +23,43 @@ function loadDoc() {
     console.log("am intrat in resetMe");
     $( "#sortable" ).sortable( "cancel" );
     //$( "#sortable" ).sortable( "refresh" );
+  }
+
+  function applyPermuttation(element_id, oldIndex, newIndex){
+    if (oldIndex > newIndex) {
+      temp = userdata_backend[oldIndex]
+      permutteAlltoTheRight(oldIndex,newIndex)
+      userdata_backend[newIndex] = temp
+    }
+    else{
+      temp = userdata_backend[oldIndex]
+      permutteAlltoTheLeft(oldIndex,newIndex)
+      userdata_backend[newIndex] = temp
+    }
+  
+    console.log("am intrat in applyPermuttation");
+    alert('id of Item moved = '+element_id+' old position = '+oldIndex+' new position = '+newIndex);
+   printMe()
+
+
+  }
+
+  function printMe(){
+    console.log("printMe Updated data");
+    for (i=0; i<8;i++)
+    {
+      console.log(userdata_backend[i].nume);
+
+    }
+  }
+
+  function permutteAlltoTheLeft(oldIndex, newIndex){
+    for (i=oldIndex; i<newIndex;i++)
+    {
+      userdata_backend[i]=userdata_backend[i+1]
+    }
+  }
+
+  function permutteAlltoTheRight(oldIndex, newIndex){
+
   }
