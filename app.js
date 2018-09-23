a6fbb46acb67a360ff7ec869fb6e45d2a80506f1
Diana Vulcu <vulcu.diana@gmail.com>
2018-09-23 18:40:48 +0300

    am facut optimile fotbaliste- BACKEND

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var userdata_model = ""
 var userdata_backend = ""
+var usedata_optimi = {}
 function loadDoc() {
     console.log("am intrat in loadDoc");
     userdata_model = JSON.parse(data);	
     userdata_backend = userdata_model
-    refresh()
+    refresh()   //going from BACKEND to FRONTEND
+    calcOptieme() //generating optimeme
   }
   function refresh(){
   for (i=0; i<32;i++)
@@ -35,12 +37,11 @@ function loadDoc() {
       temp = userdata_backend[oldIndex]
       permutteAlltoTheLeft(oldIndex,newIndex)
       userdata_backend[newIndex] = temp
-    }
-  
+    } 
     console.log("am intrat in applyPermuttation");
     alert('id of Item moved = '+element_id+' old position = '+oldIndex+' new position = '+newIndex);
    printMe()
-
+   calcOptieme()
 
   }
 
@@ -67,3 +68,29 @@ function loadDoc() {
     }
 
   }
+
+  function calcOptieme(){
+    var indexOptimi = 0;
+    for(i=0;i<32;i++){
+        if((i%4==0)||(i%4==1)){
+          usedata_optimi[indexOptimi]=userdata_backend[i];
+          indexOptimi++;
+        }
+      }   
+    reoderOptieme() 
+    console.log("calcOptimi print");
+    for(i=0;i<16;i++){
+      console.log(usedata_optimi[i].nume);
+     } 
+    }
+
+   function reoderOptieme(){
+    for(i=0;i<16;i++){
+      if(i%4==1){
+      temp = usedata_optimi[i]
+      usedata_optimi[i]=usedata_optimi[i+2]
+      usedata_optimi[i+2] = temp
+      }
+     }  
+   } 
+
